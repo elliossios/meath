93392cfa6411fad3a7d8c8316fc018277d873f86
Valentin Zambelli <valentin.zambelli@gmail.com>
2018-03-27 18:29:33 +0200

    added loop to bash function with 1 second sleep

--- start_relay.sh
+++ start_relay.sh
@@ -1,4 +1,8 @@
-python start_tracker.py --mock --measurement humidity| ./wclient humidity
-python start_tracker.py --mock --measurement acceleration| ./wclient acceleration
-python start_tracker.py --mock --measurement pressure| ./wclient pressure
-python start_tracker.py --mock --measurement temperature| ./wclient temperature
+while true; do 
+    python start_tracker.py --mock --measurement humidity| ./wclient humidity
+    python start_tracker.py --mock --measurement acceleration| ./wclient acceleration
+    python start_tracker.py --mock --measurement pressure| ./wclient pressure
+    python start_tracker.py --mock --measurement temperature| ./wclient temperature
+    sleep 1
+done
+
