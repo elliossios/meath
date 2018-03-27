3461f15415a8000b20d935a095a4cdca24941059
Valentin Zambelli <valentin.zambelli@gmail.com>
2018-03-27 18:24:31 +0200

    cleanup

--- client.c
+++ client.c
@@ -8,48 +8,18 @@ int main( int argc, char *argv[] )
 	//on which topic you want send your data?
 	char topic[16];
 	sscanf(argv[1],"%s", topic);
-
 	int topic_len = strlen(topic);
 
 	//the your hex-coded data
-    unsigned char str[150];
-
+    unsigned char str[80];
 	printf("Reading data stream\n");
 	scanf("%s", str);
-	
+
+	// Print for now
 	printf("%s\n", topic);
 	printf("%s\n", str);	
-	//set your price and how many datasets you want to trade with
-	//int price = 50;
-	//int amount = 1;
 
-
-	//float b = 0.0f;
-	//float c = 0.0f;
-	//float d = 0.0f;
-
-	// Beware argv[0] is program name
-	//sscanf(argv[1],"%f", &a);
-	//sscanf(argv[2],"%f", &b);
-	//sscanf(argv[3],"%f", &c);
-	//sscanf(argv[4],"%f", &d);
-	//printf("%s", argv[1]);
-
-	//printf("a is: %f\n", a);
-	//printf("b is: %f\n", b);
-	//printf("c is: %f\n", c);
-	//printf("d is: %f\n", d);
-
-	//float abcd[4] = { a, b, c, d };
-
-	//char *data = malloc(sizeof(float) * 4);
-	//u_int8_t *float_data = (u_int8_t*) &abcd;
-	
-	// int i;
-	// for(i=0;i<4;i++)
-	// {
-	// //producer(topic, topic_len, price, amount, data, data_len);
-	    /*set strH with nulls*/
+	//producer(topic, topic_len, price, amount, data, data_len);
 
 	return 0;
 }
