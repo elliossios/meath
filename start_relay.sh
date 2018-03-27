1ee4969a5d9370b17083ce941c9b6ab665f79ac9
Valentin Zambelli <valentin.zambelli@gmail.com>
2018-03-27 18:57:40 +0200

    cleanup

--- start_relay.sh
+++ start_relay.sh
@@ -1,8 +1,8 @@
 while true; do 
-    python start_tracker.py --mock --measurement humidity| ./wclient humidity
-    python start_tracker.py --mock --measurement acceleration| ./wclient acceleration
-    python start_tracker.py --mock --measurement pressure| ./wclient pressure
-    python start_tracker.py --mock --measurement temperature| ./wclient temperature
+    python start_tracker.py --mock --measurement humidity | ./wclient humidity
+    python start_tracker.py --mock --measurement acceleration | ./wclient acceleration
+    python start_tracker.py --mock --measurement pressure | ./wclient pressure
+    python start_tracker.py --mock --measurement temperature | ./wclient temperature
     sleep 1
 done
 
--- start_tracker.py
+++ start_tracker.py
@@ -7,22 +7,8 @@ from tracker.tracker_mock import MockSensor
 from signal import signal, SIGPIPE, SIG_DFL 
 
 SENSOR_MAC = 'C3:D4:CC:7C:6C:2E'
-LOCK = "tracker.lock"
 
-def toHex(s):
-    lst = []
-    for ch in s:
-        hv = hex(ord(ch)).replace('0x', '')
-        if len(hv) == 1:
-            hv = '0'+hv
-        lst.append(hv)
-    
-    return reduce(lambda x,y:x+y, lst)
-
-def toStr(s):
-    return s and chr(atoi(s[:2], base=16)) + toStr(s[2:]) or ''
-
-def recreate_file(filename="tracker_log.csv"):
+def create_log_file(filename="tracker_log.csv"):
     try:
         os.remove(filename)
     except OSError:
@@ -31,23 +17,26 @@ def recreate_file(filename="tracker_log.csv"):
     with open(filename, "w+") as f:
         f.write("timestamp,acceleration,pressure,temperature,humidity\n")
 
-def main(sensor, measurement):
+def write_to_csv(state):
+    with open("tracker_log.csv", "a") as f:
+        data = "{},{},{},{},{}".format(
+            state["timestamp"],
+            state["acceleration"],
+            state["pressure"],
+            state["temperature"],
+            state["humidity"]
+        )
+        f.write(data + "\n")
+
+def run(sensor, measurement):
     signal(SIGPIPE,SIG_DFL) 
 
-    state = sensor.update()
+    _ = sensor.update()
     state = sensor.state
-    data = ""
+    state["timestamp"] = time.time()
+
     try:
-        with open("tracker_log.csv", "a") as f:
-            state["timestamp"] = time.time()
-            data = "{},{},{},{},{}".format(
-                state["timestamp"],
-                state["acceleration"],
-                state["pressure"],
-                state["temperature"],
-                state["humidity"]
-            )
-            f.write(data + "\n")
+        #write_to_csv(state)
         data = "{},{}".format(
             state["timestamp"],
             state[measurement]
@@ -64,10 +53,12 @@ if __name__=="__main__":
     parser.add_argument('--mock', action='store_false')
     parser.add_argument('--measurement')
     args = parser.parse_args()
+
+    # Check if we want to mock the sensor because we are not running on HiKey
     if args.mock:
         sensor = RuuviTag(SENSOR_MAC)
     else:
         sensor = MockSensor()
 
-    recreate_file()
-    main(sensor, args.measurement)
+    create_log_file()
+    run(sensor, args.measurement)
