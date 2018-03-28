f1f31375b0f5873e7dcd9ba8368d6dd74e7a68a1
Valentin Zambelli <valentin.zambelli@gmail.com>
2018-03-28 15:44:59 +0200

    setup env for hikey

---
+++ setup_env.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install python3-dev python3-psutil
+sudo apt-get install bluez bluez-hcidump
+sudo pip3 install --upgrade setuptools
+pip3 install --user -r requirements.txt
