91a2751516172513fc1e182177aaba99ad5ee8d5
Valentin Zambelli <valentin.zambelli@gmail.com>
2018-03-28 14:38:32 +0200

    update to bash

--- start_reli.sh
+++ start_reli.sh
@@ -1,8 +1,8 @@
 while true; do 
-    python start_tracker.py --mock --measurement humidity | ./wclient humidity
-    python start_tracker.py --mock --measurement acceleration | ./wclient acceleration
-    python start_tracker.py --mock --measurement pressure | ./wclient pressure
-    python start_tracker.py --mock --measurement temperature | ./wclient temperature
+    python3 start_tracker.py --mock --measurement humidity | hackathon_client humidity
+    python3 start_tracker.py --mock --measurement acceleration | hackathon_client acceleration
+    python3 start_tracker.py --mock --measurement pressure | hackathon_client pressure
+    python3 start_tracker.py --mock --measurement temperature | hackathon_client temperature
     sleep 1
 done
 
