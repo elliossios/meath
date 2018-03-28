921fed144da06a62a4e96c6754a43fb734828fd8
Valentin Zambelli <valentin.zambelli@gmail.com>
2018-03-28 15:22:02 +0200

    update

--- start_reli.sh
+++ start_reli.sh
@@ -3,6 +3,6 @@ while true; do
     python3 start_tracker.py --mock --measurement acceleration | hackathon_client acceleration
     python3 start_tracker.py --mock --measurement pressure | hackathon_client pressure
     python3 start_tracker.py --mock --measurement temperature | hackathon_client temperature
-    sleep 1
+    sleep 10
 done
 
